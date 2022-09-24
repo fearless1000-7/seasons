a9d944d85c99f08f4a9aec968d4cd539f80f3532
fearless1000-7 <112949490+fearless1000-7@users.noreply.github.com>
2022-09-24 16:01:00 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let month1 = prompt('Укажте месяц')
+ if(month1 >= 13 ){
+    alert('eror')
+}
+else if(month1 ==='1'){
+   alert('winter')
+} 
+
+ else if(month1 ==='2'){
+     alert('winter')
+ } 
+ else if(month1 ==='3'){
+    alert('spring')
+ }
+ else if(month1 ==='4'){
+    alert('spring')
+ }
+ else if(month1 ==='5'){
+    alert('spring')
+ }
+ else if(month1 ==='6'){
+    alert('summer')
+ }
+ else if(month1 ==='7'){
+    alert('summer')
+ }
+ else if(month1==='8'){
+    alert('summer')
+ }
+ else if(month1 ==='9'){
+    alert('autumn')
+ }
+ else if(month1 ==='10'){
+    alert('autumn')
+ }
+ else if(month1 ==='11'){
+    alert('autumn')
+ }
+ else if(month1 ==='12'){
+    alert('winter')
+ }
+
+
+ 
+
+ 
